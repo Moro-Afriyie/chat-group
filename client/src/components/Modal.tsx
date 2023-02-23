f5a1a9d6f562459d509294d71e914f40e982f515
captainakaps1 <brightakakpo40@gmail.com>
2023-02-23 20:10:05 +0000

    clickoutside

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -5,10 +5,40 @@ interface IModal {
   children: ReactNode;
 }
 
+interface IOutsideClick {
+  closeModal: (isOpen: boolean) => void;
+}
+
+const useOutsideClick = ({
+  closeModal,
+}: IOutsideClick): React.RefObject<HTMLDivElement> => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      closeModal(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  return modalRef;
+};
+
 const Modal: React.FC<IModal> = ({ children, closeModal }) => {
+  const modalRef = useOutsideClick({ closeModal: closeModal });
+
   return (
     <div className="fixed top-0 left-0 right-0 w-screen h-screen z-50 overflow-x-hidden overflow-y-auto flex items-center justify-center bg-gray-700 bg-opacity-60">
-      <div className="bg-dark w-9/12 md:w-1/3 p-8 text-white rounded-2xl">
+      <div
+        className="bg-dark max-w-[30rem] w-full mx-4 p-8 text-white rounded-2xl"
+        ref={modalRef}
+      >
         {children}
       </div>
     </div>
